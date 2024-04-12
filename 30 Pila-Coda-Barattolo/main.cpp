0f7aa4668307f09821cd3b91d5fbf776c50e4743
FullMetalPie <andrea.pietri06@gmail.com>
2024-04-12 20:30:59 +0200

    Updated main file

--- 30 Pila-Coda-Barattolo/main.cpp
+++ 30 Pila-Coda-Barattolo/main.cpp
@@ -3,6 +3,21 @@
 
 using namespace std;
 
+/*
+Realizzare un programma che, avvalendosi di opportune classi,
+risolva il seguente problema:
+ 
+In un supermercato ogni barattolo di marmellata è memorizzato registrando
+il suo nome, il suo codice, la data di scadenza, la data di arrivo e il suo prezzo.
+
+Le marmellate vengono inserite in una pila, in ordine di arrivo cronologico 
+(per data di arrivo). Ricavare, dalla pila, una coda che memorizzi le marmellate 
+in ordine FIFO di scadenza (l'ultima marmellata inserita in coda è quella con data
+di scadenza maggiore). Il programma deve essere in grado di fornire, 
+su richiesta dell'utente, il codice della prossima marmellata da consumare, 
+cioè quella con data di scadenza minore.
+*/
+
 int main () {
     Barattolo b;
 
